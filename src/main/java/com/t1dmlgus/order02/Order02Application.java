5fa8d74b79733aba727a81d4c15fa27f44b92b61
t1dmlgus <dmlgusgngl@gmail.com>
2022-05-31 12:20:39 +0900

    Initial commit

---
+++ src/main/java/com/t1dmlgus/order02/Order02Application.java
@@ -0,0 +1,13 @@
+package com.t1dmlgus.order02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Order02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Order02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/t1dmlgus/order02/Order02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t1dmlgus.order02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Order02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
